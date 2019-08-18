3a679da3cfd76e8950068c784de663141d5d63c1
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-18 18:54:06 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -24,7 +24,11 @@ def second_challenge
         :name => "Lady Capulet",
         :age => "51"
       },
-      :heroine =>{},
+      :heroine =>{
+        :name => "Juliet",
+        :age => "15"
+        :status => "alive"
+      },
       :heroine_friends =>[]
       #your key/value pairs here!
       }}
