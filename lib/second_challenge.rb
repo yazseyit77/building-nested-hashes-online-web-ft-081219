65e4719316d4ab4f979046ad7873fc7ac37857d5
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-18 18:48:40 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,10 +2,10 @@ def second_challenge
   epic_tragedy = {
     :montague => {:patriarch => {}, :matriarch =>{}, :hero{}, :hero_friends[]
       #your key/value pairs here!
-      }, 
+      },
     :capulet => {:patriarch => {}, :matriarch =>{}, :hero{}, :heroine_friends[]
       #your key/value pairs here!
       }}
 
-  
+
 end
