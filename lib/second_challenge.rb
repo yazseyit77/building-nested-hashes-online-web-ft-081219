922a7af2f1e6b0d88fcb4d8f0451403321c372a8
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-18 18:52:54 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -7,7 +7,11 @@ def second_challenge
       :matriarch =>{
         :name => "Lady Montague",
         :age => "54"},
-      :hero =>{},
+      :hero =>{
+        :name => "Romeo",
+        :age => "15"
+        :status => "alive"
+      },
       :hero_friends =>[]
       #your key/value pairs here!
       },
