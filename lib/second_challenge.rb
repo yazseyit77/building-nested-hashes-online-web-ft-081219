502a3879d48c2e7958005bd3c8a5cf6934b76c66
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-18 18:56:28 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,34 +1,16 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriarch =>{
-        :name => "Lord Montague",
-        :age => "53"},
-      :matriarch =>{
-        :name => "Lady Montague",
-        :age => "54"},
-      :hero =>{
-        :name => "Romeo",
-        :age => "15",
-        :status => "alive"
-      },
+      :patriarch =>{},
+      :matriarch =>{},
+      :hero =>{},
       :hero_friends =>[]
       #your key/value pairs here!
       },
     :capulet => {
-      :patriarch =>{
-        :name => "Lord Capulet",
-        :age => "50"
-      },
-      :matriarch =>{
-        :name => "Lady Capulet",
-        :age => "51"
-      },
-      :heroine =>{
-        :name => "Juliet",
-        :age => "15",
-        :status => "alive"
-      },
+      :patriarch =>{},
+      :matriarch =>{},
+      :heroine =>{},
       :heroine_friends =>[]
       #your key/value pairs here!
       }}
